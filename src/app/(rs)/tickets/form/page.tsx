9323c9423e649da3ef6e6c30bd56815e06d36225
akhil683 <onkar.palsra@gmail.com>
2024-11-11 19:37:25 +0530

    feat: get queries

---
+++ src/app/(rs)/tickets/form/page.tsx
@@ -0,0 +1,89 @@
+import { BackButton } from "@/components/BackButton";
+import { getCustomer } from "@/lib/queries/getCustomer";
+import { getTicket } from "@/lib/queries/getTicket";
+import * as Sentry from "@sentry/nextjs"
+
+export default async function TicketFormPage({
+  searchParams,
+
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+  try {
+    const { customerId, ticketId } = await searchParams
+
+    if (!customerId && !ticketId) {
+      return (
+        <>
+          <h2 className="text-2xl mb-2 text-white">
+            Ticket id or Cusomter Id required to load ticket form
+          </h2>
+          <BackButton
+            title="Go Back"
+            variant={"default"}
+          />
+        </>
+      )
+    }
+    //New Ticket form
+    if (customerId) {
+      const customer = await getCustomer(parseInt(customerId))
+      if (!customer) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer Id: #{customerId} not found
+            </h2>
+            <BackButton
+              title="Go Back"
+              variant={"default"}
+            />
+          </>
+        )
+      }
+      if (!customer.active) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer Id: #{customerId} is Not Active
+            </h2>
+            <BackButton
+              title="Go Back"
+              variant={"default"}
+            />
+          </>
+        )
+      }
+      //return ticket form
+      console.log(customer)
+    }
+    //Edit ticket form
+    if (ticketId) {
+      const ticket = await getTicket(parseInt(ticketId))
+
+      if (!ticket) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Ticket Id: #{ticketId} not found
+            </h2>
+            <BackButton
+              title="Go Back"
+              variant={"default"}
+            />
+          </>
+        )
+      }
+      const customer = await getCustomer(ticket.customerId)
+      //return ticket form
+
+      console.log("ticket: ", ticket)
+      console.log("customer: ", customer)
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      Sentry.captureException(e)
+      throw e
+    }
+  }
+}
