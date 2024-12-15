c3352801059985e5a5c0b61370fd8f84a3af1f42
akhil683 <onkar.palsra@gmail.com>
2024-12-16 02:02:09 +0530

    feat: tanstack table with search

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,5 +1,7 @@
-import CustomerSearch from "./CustomerSearch";
+import * as Sentry from "@sentry/nextjs"
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerResults";
+import CustomerSearch from "./CustomerSearch";
+import CustomerTable from "./CustomerTable";
 
 export const metadata = {
   title: "Customer Search",
@@ -14,14 +16,23 @@ export default async function Customers({
   const { searchText } = await searchParams
   if (!searchText) return <CustomerSearch />
 
-  //query database
+
+  //query database: Analyse performance with sentry
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-2"
+  })
   const results = await getCustomerSearchResults(searchText)
+  span.end()
 
   //return results
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length
+        ? <CustomerTable data={results} />
+        : <p className="mt-6 text-lg">No Results Found</p>
+      }
+
     </>
   )
 }
