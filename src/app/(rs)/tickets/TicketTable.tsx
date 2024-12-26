9ec55b82e2d6fbffef5b3ac57c7dd047bf5bcd79
akhil683 <onkar.palsra@gmail.com>
2024-12-27 02:42:00 +0530

    fix: sync pagination & filters

--- src/app/(rs)/tickets/TicketTable.tsx
+++ src/app/(rs)/tickets/TicketTable.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useMemo } from "react"
+import { useState, useMemo, useEffect } from "react"
 import type { TicketSearchResultsType } from "@/lib/queries/getTicketSearchResults"
 import { useRouter, useSearchParams } from "next/navigation"
 
@@ -156,6 +156,17 @@ export default function TicketTable({ data }: Props) {
     getSortedRowModel: getSortedRowModel(),
   })
 
+  //sync filters with pagination
+  useEffect(() => {
+    const currentPageIndex = table.getState().pagination.pageIndex
+    const pageCount = table.getPageCount()
+    if (pageCount <= currentPageIndex && currentPageIndex > 0) {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set("page", "1")
+      router.replace(`${params.toString()}`, { scroll: false })
+    }
+  }, [table.getState().columnFilters]) //eslint-disable-line  react-hooks/exhaustive-deps
+
   return (
     <div className="mt-6 flex flex-col gap-4">
       <div className="rounded-lg overflow-hidden border border-border">
@@ -219,7 +230,7 @@ export default function TicketTable({ data }: Props) {
       <div className="flex justify-between gap-2 flex-wrap items-center">
         <div className="flex basis-1/3 items-center">
           <p className="whitespace-nowrap font-bold">
-            {`Page ${table.getState().pagination.pageIndex + 1} of ${table.getPageCount()}`}
+            {`Page ${table.getState().pagination.pageIndex + 1} of ${Math.max(1, table.getPageCount())}`}
             &nbsp;&nbsp;
             {`${table.getFilteredRowModel().rows.length} ${table.getFilteredRowModel().rows.length !== 1 ? "total results" : "result"}`}
           </p>
